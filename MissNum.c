828589005a6b36e5664f92645fd1a9d3a8ee48d5
MiLkTeA <43979198+18Grace18@users.noreply.github.com>
2019-11-05 09:48:55 +0700

    Update MissNum.c

--- MissNum.c
+++ MissNum.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
-int miss(int a[],int n) {
-  int i,sum1=0,sum2=0;
-  sum1=(n+1)*(n+2)/2; 
-  for(i=0;i<n;i++) {
-    sum2+=a[i];
-  }
-  return sum1-sum2;
-}
 int main() {
-  int n,i;
+  int n,i,j,t,c;
   scanf("%d",&n);
   int a[n];
   for(i=0;i<n;i++) {
     scanf("%d",&a[i]);
   }
-  printf("%d",miss(a,n));
+  for(i=0;i<n;i++) {
+    for(j=0;j<n-1;j++) {
+    if(a[j]==a[j+1]) {
+      t=a[j];
+      a[j]=a[j+1];
+      a[j+1]=t;
+      }
+    }
+  }
+  for(j=a[0];j<a[n-1];j++) {
+    c=0;
+    for(i=0;i<n;i++) {
+      if(a[i]==j)
+      c++;
+    }
+    if(c==0) {
+      printf("%d ",j);
+    }
+  }
   return 0;
 }
